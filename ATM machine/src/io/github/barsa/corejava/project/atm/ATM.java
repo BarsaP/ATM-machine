c861370647c706ce5833673ac388a6863c86a92a
barsa <jenaharapriya8637@gmail.com>
2023-07-11 19:32:35 +0530

    Initial Commit

---
+++ ATM machine/src/io/github/barsa/corejava/project/atm/ATM.java
@@ -0,0 +1,15 @@
+package io.github.barsa.corejava.project.atm;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args) throws IOException {
+		OptionMenu optionmenu = new OptionMenu();
+		
+		optionmenu.getLogin();
+		
+
+	}
+
+}
